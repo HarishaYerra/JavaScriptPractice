4331139917f60e035afb1232cb2b0389d2ea018b
YERRA HARISHA <harishayerra2001@gmail.com>
2026-05-25 11:43:14 +0530

    added tasks

--- Test.js
+++ Test.js
@@ -1,2 +1,17 @@
-const a="Hello"
-console.log(a)
+//Task1
+const name="Harisha"
+const Company = "TCS"
+const experiance= 2
+console.log("My name is "+name+ " working at " +Company+ " with " +experiance+ " year experiance")
+
+//Task2-function
+function findSquare(n){
+    return n*n;
+}
+console.log(findSquare(5));
+
+//Task3- Arrow Function
+const subtract=(a,b)=>{
+    return a-b;
+}
+console.log(subtract(10,5));
